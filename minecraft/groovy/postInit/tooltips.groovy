84384aec25ef066548fe1f40af059b778f9028a6
Levviata <68709348+Levviata@users.noreply.github.com>
2026-08-03 19:12:33 +0200

    Add tooltips for Radiation Absorbers and Muffler blocks

--- minecraft/groovy/postInit/tooltips.groovy
+++ minecraft/groovy/postInit/tooltips.groovy
@@ -1,7 +1,7 @@
 import net.minecraftforge.event.entity.player.ItemTooltipEvent
 
 def deprecatedNotice = '(LEGACY)'
-// More than once that I see an item that has no use and that has been deprecated, but it is not declared.
+// More than once I see an item that has no use that has been deprecated without the "(LEGACY)".
 // here comes the LevviataInc fix.
 event_manager.listen { ItemTooltipEvent event ->
     if (event.getItemStack() in item('hbm:hadron_coil_mese')) {
@@ -12,5 +12,24 @@ event_manager.listen { ItemTooltipEvent event ->
     }
     if (event.getItemStack() in item('hbm:machine_fensu')) {
         event.getToolTip() << deprecatedNotice
+    }
+    if (event.getItemStack() in item('hbm:rad_absorber')) { // cant remove that special § letter from the tooltip, lame
+        event.getToolTip() << "§e Removes radiation from the current chunk."
+        event.getToolTip() << "§e -5 chunk rad / second"
+    }
+    if (event.getItemStack() in item('hbm:rad_absorber:1')) {
+        event.getToolTip() << "§e Removes radiation from the current chunk."
+        event.getToolTip() << "§e -20 chunk rad / second"
+    }
+    if (event.getItemStack() in item('hbm:rad_absorber:2')) {
+        event.getToolTip() << "§e Removes radiation from the current chunk."
+        event.getToolTip() << "§e -200 chunk rad / second"
+    }
+    if (event.getItemStack() in item('hbm:rad_absorber:3')) {
+        event.getToolTip() << "§e Removes radiation from the current chunk."
+        event.getToolTip() << "§e -20000 chunk rad / second"
+    }
+    if (event.getItemStack() in item('hbm:muffler')) {
+        event.getToolTip() << "§e Probably useless."
     }
 }
