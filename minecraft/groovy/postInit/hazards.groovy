8069be8e294c18c0e921d3a36fa84b9219c22b20
Levviata <68709348+Levviata@users.noreply.github.com>
2026-09-02 04:29:16 +0200

    Add explosive hazard to ullapool caber

--- minecraft/groovy/postInit/hazards.groovy
+++ minecraft/groovy/postInit/hazards.groovy
@@ -2,3 +2,5 @@ import com.hbm.hazard.*;
 
 HazardSystem.register(item("hbm:coal_infernal"), HazardRegistry.makeData(HazardRegistry.HOT, 3f));
 HazardSystem.register(item("hbm:powder_fire"), HazardRegistry.makeData(HazardRegistry.HOT, 2f));
+
+HazardSystem.register(item("hbm:ullapool_caber"), HazardRegistry.makeData(HazardRegistry.EXPLOSIVE, 5f));
