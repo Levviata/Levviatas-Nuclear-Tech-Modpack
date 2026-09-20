81ef2e53709b118109913efff8b8ae90fd67218c
Levviata <68709348+Levviata@users.noreply.github.com>
2026-09-20 06:51:10 +0200

    remove event it dont work

--- minecraft/groovy/postInit/events.groovy
+++ minecraft/groovy/postInit/events.groovy
@@ -1,10 +1,15 @@
 import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent
-
+/* doesnt work
 event_manager.listen { FurnaceFuelBurnTimeEvent event ->
-    if (event.getItemStack().getItem() == item("hbm:block_scrap")) {
+    def stack = event.getItemStack()
+
+    if (stack == item("hbm:block_scrap")) {
         event.setBurnTime(0)
+        log("setting block scrap burn time to 0")
     }
-    if (event.getItemStack().getItem() == item("hbm:scrap")) {
+
+    if (stack == item("hbm:scrap")) {
         event.setBurnTime(0)
+        log("setting scrap burn time to 0")
     }
-}
+}*/
