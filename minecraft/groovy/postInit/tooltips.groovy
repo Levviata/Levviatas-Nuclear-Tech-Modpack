029b3c327d60d2c3d42fe58aa9f0a0c76344a955
Levviata <68709348+Levviata@users.noreply.github.com>
2026-09-25 14:25:39 +0200

    give crates their drops as tooltips

--- minecraft/groovy/postInit/tooltips.groovy
+++ minecraft/groovy/postInit/tooltips.groovy
@@ -106,5 +106,90 @@ event_manager.listen { ItemTooltipEvent event ->
         event.getToolTip() << slidingblastdoor1
         event.getToolTip() << slidingblastdoor2
     }
+    if (event.getItemStack() in item('hbm:crate')) {
+        event.getToolTip() << 'Drops (3-5)'
+        event.getToolTip() << '- Stimpak (28.57%)'
+        event.getToolTip() << '- Antidote (14.29%)'
+        event.getToolTip() << '- HE Grenade (22.86%)'
+        event.getToolTip() << '- Incendiary Grenade (11.43%)'
+        event.getToolTip() << '- HE Stick Grenade (17.14%)'
+        event.getToolTip() << '- Ammo Container (5.71%)'
+    }
+    if (event.getItemStack() in item('hbm:crate_weapon')) {
+        event.getToolTip() << 'Drops (1-2. 1% for 25)'
+        event.getToolTip() << '- Break-Action Revolver (26.32%)'
+        event.getToolTip() << "- Lever Action Shotgun (18.42%)"
+        event.getToolTip() << '- Heavy Revolver (13.16%)'
+        event.getToolTip() << '- Grease Gun (13.16%)'
+        event.getToolTip() << '- Liberator (5.26%)'
+        event.getToolTip() << '- Flare Gun (21.05%)'
+        event.getToolTip() << '- Panzerschreck (2.63%)'
+    }
+    if (event.getItemStack() in item('hbm:crate_lead')) {
+        event.getToolTip() << 'Drops (3-5)'
+        event.getToolTip() << '- Uranium Ingot (5.62%)'
+        event.getToolTip() << '- U-238 Ingot (4.49%)'
+        event.getToolTip() << '- Plutonium Ingot (3.93%)'
+        event.getToolTip() << '- Pu-240 Ingot (3.37%)'
+        event.getToolTip() << '- Neptunium Ingot (3.93%)'
+        event.getToolTip() << '- Uranium Fuel Ingot (4.49%)'
+        event.getToolTip() << '- Plutonium Fuel Ingot (3.93%)'
+        event.getToolTip() << '- MOX Fuel Ingot (3.37%)'
+        event.getToolTip() << '- Uranium Nugget (5.62%)'
+        event.getToolTip() << '- U-238 Nugget (4.49%)'
+        event.getToolTip() << '- Plutonium Nugget (3.93%)'
+        event.getToolTip() << '- Pu-240 Nugget (3.37%)'
+        event.getToolTip() << '- Neptunium Nugget (3.93%)'
+        event.getToolTip() << '- Uranium Fuel Nugget (4.49%)'
+        event.getToolTip() << '- Plutonium Fuel Nugget (3.93%)'
+        event.getToolTip() << '- MOX Fuel Nugget (3.37%)'
+        event.getToolTip() << '- Deuterium Cell (4.49%)'
+        event.getToolTip() << '- Tritium Cell (4.49%)'
+        event.getToolTip() << '- UF6 Cell (4.49%)'
+        event.getToolTip() << '- PUF6 Cell (4.49%)'
+        event.getToolTip() << '- RTG Pellet (3.37%)'
+        event.getToolTip() << '- Weak RTG Pellet (3.93%)'
+        event.getToolTip() << '- Tritium-Deuterium Cake (2.81%)'
+        event.getToolTip() << '- Yellowcake Powder (5.62%)'
+    }
+    if (event.getItemStack() in item('hbm:crate_metal')) {
+        event.getToolTip() << 'Drops (3-5)'
+        event.getToolTip() << '- Burner Press (7.81%)'
+        event.getToolTip() << '- Breeding Reactor (4.69%)'
+        event.getToolTip() << '- Wood-Burning Generator (7.81%)'
+        event.getToolTip() << '- Diesel Generator (6.25%)'
+        event.getToolTip() << '- RT Generator (3.12%)'
+        event.getToolTip() << '- Electricity Pylon (7.03%)'
+        event.getToolTip() << '- Energy Storage Block (6.25%)'
+        event.getToolTip() << '- Li-Ion Energy Storage Block (3.91%)'
+        event.getToolTip() << '- Electric Furnace (6.25%)'
+        event.getToolTip() << '- Assembly Machine (7.81%)'
+        event.getToolTip() << '- Tank (5.47%)'
+        event.getToolTip() << '- Centrifuge Element (4.69%)'
+        event.getToolTip() << '- Motor (6.25%)'
+        event.getToolTip() << '- Heating Coil (5.47%)'
+        event.getToolTip() << '- Photovoltaic Panel (2.34%)'
+        event.getToolTip() << '- Copper Coil (7.81%)'
+        event.getToolTip() << '- Titanium Blade (2.34%)'
+        event.getToolTip() << '- Combustion Engine Piston (4.69%)'
+    }
+    /*
+    if (event.getItemStack() in item('hbm:crate_red')) {
+        event.getToolTip() << 'Drops (14)'
+        event.getToolTip() << '- Brittle Spade (7.14%)'
+        event.getToolTip() << "- Little Macintosh (7.14%)"
+        event.getToolTip() << '- Sexy (7.14%)'
+        event.getToolTip() << "- Broken (7.14%)"
+        event.getToolTip() << '- M44 Equestrian Ammo (7.14%)'
+        event.getToolTip() << '- G12 Equestrian Ammo (7.14%)'
+        event.getToolTip() << '- .50 BMG Demolisher (7.14%)'
+        event.getToolTip() << '- Spark Battery (7.14%)'
+        event.getToolTip() << '- Sparkle Bottle (7.14%)'
+        event.getToolTip() << '- Rad Bottle (7.14%)'
+        event.getToolTip() << '- Starmetal Ring (7.14%)'
+        event.getToolTip() << '- Flame Pony (7.14%)'
+        event.getToolTip() << '- NTM Dirt (7.14%)'
+        event.getToolTip() << '- Broadcaster PC (7.14%)'
+    }*/
 }
 log.info("Loaded tooltips!")
